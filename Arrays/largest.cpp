30d39320bcf8fa5b0b21da0a2a7ac161612a4d79
Shahil Patel <shahilptl18@outlook.com>
2026-06-07 08:37:50 +0530

    largest no

--- Arrays/largest.cpp
+++ Arrays/largest.cpp
@@ -4,19 +4,22 @@ using namespace std;
 
 int main()
 {
-    int nums[] = {10, 22, 5, 11, -9};
+    int nums[] = {100, 22, 5, 11, -9};
     int size = 5;
 
     int largest = INT_MIN;
+    int index = -1;
 
     for (int i = 0; i < size; i++)
     {
         if (nums[i] > largest)
         {
             largest = nums[i];
+            index = i;
         }
     }
-    cout << "largest no : " << largest;
+    cout << "largest no : " << largest<<endl;
+    cout<<"index : "<<index;
 
     return 0;
 }

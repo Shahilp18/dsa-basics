c0d6f5c58c2aca8c51ef5360682050670696c23f
Shahil Patel <shahilptl18@outlook.com>
2026-06-07 08:20:15 +0530

    array loop

---
+++ Arrays/basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks[3] = {100, 90, 80};
+    int size = 3;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout<<marks[i]<<endl;
+    }
+    
+    return 0;
+}
